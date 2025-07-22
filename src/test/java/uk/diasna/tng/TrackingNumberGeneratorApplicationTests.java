87cd31f341d5c7440a601e8b5cdb1553680f373b
diasna <dias.arifin@gmail.com>
2025-07-22 08:47:46 +0700

    init

---
+++ src/test/java/uk/diasna/tng/TrackingNumberGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.diasna.tng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingNumberGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
